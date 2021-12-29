f24d6e628441f4db96659b70328768aaaac1960e
Jonathan Moraes <jonathan.pmoraes@senacsp.edu.br>
2021-12-29 15:41:54 -0300

    Initial commit

---
+++ src/ComplexNumber.java
@@ -0,0 +1,37 @@
+public class ComplexNumber {
+
+    private double real;
+    private double imaginary;
+
+    public ComplexNumber(double real, double imaginary){
+
+        this.real = real;
+        this.imaginary = imaginary;
+
+    }
+
+    public double getReal(){
+        return this.real;
+    }
+    public double getImaginary(){
+        return this.imaginary;
+    }
+
+    public void add(double real, double imaginary){
+        this.real = this.real + real;
+        this.imaginary = this.imaginary + imaginary;
+    }
+    public void add(ComplexNumber complexNumber){
+        real = real + complexNumber.getReal();
+        imaginary = imaginary + complexNumber.getImaginary();
+    }
+    public void subtract(double real, double imaginary){
+        this.real = this.real - real;
+        this.imaginary = this.imaginary - imaginary;
+    }
+    public void subtract(ComplexNumber complexNumber){
+        real = real - complexNumber.getReal();
+        imaginary = imaginary - complexNumber.getImaginary();
+    }
+
+}
